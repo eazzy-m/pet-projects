1a33ce6bc21dad7a1a16cc75afc2e0305f5c89ba
echa36 <teheton@mail.ru>
2021-04-11 15:54:18 +0300

    do ContentType in file models

--- catalog/models.py
+++ catalog/models.py
@@ -14,17 +14,17 @@ class ShowGoodsManager:
     def get_goods_for_main_page(*args, **kwargs):
         with_respect_to = kwargs.get('with_respect_to')
         goods = []
-        ct_models = ContentType.objects.filter(models__in=args)
+        ct_models = ContentType.objects.filter(model__in=args)
         for ct_model in ct_models:
             model_goods = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
             goods.extend(model_goods)
-        # if with_respect_to:
-        #     ct_model = ContentType.objects.filter(models=with_respect_to)
-        #     if ct_model.exists():
-        #         if with_respect_to in args:
-        #             return sorted(
-        #                 products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True
-        #             )
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(
+                        goods, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True
+                    )
         return goods
 
 
